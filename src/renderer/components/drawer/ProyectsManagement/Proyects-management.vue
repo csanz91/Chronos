eefd400677e7762c841fec54c8941f014e03959a
Cesar Sanz <cesarsanz.91@gmail.com>
2020-04-27 19:13:42 +0200

    Formatting

--- src/renderer/components/drawer/ProyectsManagement/Proyects-management.vue
+++ src/renderer/components/drawer/ProyectsManagement/Proyects-management.vue
@@ -21,8 +21,8 @@
           >
             <path fill="#F6F2EB" d="M31 12h-11v-11c0-0.552-0.448-1-1-1h-6c-0.552 0-1 0.448-1 1v11h-11c-0.552 0-1 0.448-1 1v6c0 0.552 0.448 1 1 1h11v11c0 0.552 0.448 1 1 1h6c0.552 0 1-0.448 1-1v-11h11c0.552 0 1-0.448 1-1v-6c0-0.552-0.448-1-1-1z"></path>
           </svg>
+          </div>
         </div>
-    </div>
       </div>
       <proyects-management-bar v-for="proyect in allProyects" :key="proyect.proyectId" :proyect=proyect @editProyect="editProyect(proyect.proyectId)" @deleteProyect="deleteProyect(proyect.proyectId)" :onEdition="proyect.proyectId===proyectIdOnEdition" />
     </div>
